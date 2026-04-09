6e54b5651529783257ef96c74e6c0e123666ca9d
Daywaltz <sudrajatbebeng200@gmail.com>
2026-04-09 20:06:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showPage(pageId) {
+    let pages = document.querySelectorAll('.page');
+    pages.forEach(p => p.classList.remove('active'));
+
+    document.getElementById(pageId).classList.add('active');
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark');
+}
